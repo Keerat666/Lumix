4ae92ca9c6bdaf9efb41e5b1bfaf0ff7a335a896
Jasleen Sondhi <jasleensondhi@gmail.com>
2023-10-06 19:40:41 +0530

    deployment fix

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = 8000;
 
 require('dotenv').config();
 
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'ui/lumix/build')));
 
