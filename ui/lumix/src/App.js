96cf63a91c601aa0ea0ed5d41df7948395022c1b
Aditya Kumar <adityapal1409@gmail.com>
2023-10-15 00:33:29 +0530

    created button component & changed title of the project

--- ui/lumix/src/App.js
+++ ui/lumix/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          rel="noopener noreferrer"
-        >
-          Let's Build Lumix
-        </a>
-      </header>
-    </div>
-  );
+	//this function is passed as a prop to the Button component
+	const handleOnClick = () => {
+		alert("Handle onClick is called");
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" rel="noopener noreferrer">
+					Let's Build Lumix
+				</a>
+				<Button
+					name={"Click me"} //can change the button name
+					handleOnClick={handleOnClick}
+				/>
+			</header>
+		</div>
+	);
 }
 
 export default App;
