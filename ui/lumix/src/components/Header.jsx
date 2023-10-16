6838e28b0ea5bc38620ee9b5434b1cc08a262554
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-10-16 14:48:22 +0530

    added logo in the header

--- ui/lumix/src/components/Header.jsx
+++ ui/lumix/src/components/Header.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo/logo-transparent-2.png'
 
 function LumixHeader() {
   const navigate = useNavigate();
@@ -13,11 +14,13 @@ function LumixHeader() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark" sticky="top">
       <Container>
-        <Navbar.Brand onClick={() => navigate('/')} style={navLinkStyle}>Lumix</Navbar.Brand>
+        <Navbar.Brand onClick={() => navigate('/')} style={navLinkStyle}>
+        <img src={logo} alt="Lumix Logo" style={{width: '100px' }} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link onClick={() => navigate('/')} style={navLinkStyle}>Documentation</Nav.Link>
+            <Nav.Link onClick={() => navigate('/')} style={navLinkStyle}>Components</Nav.Link>
             <Nav.Link onClick={() => navigate('/contributors')} style={navLinkStyle}>Contributors</Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- ui/lumix/src/pages/contributors/index.js
+++ ui/lumix/src/pages/contributors/index.js
@@ -10,8 +10,6 @@ const Contributors = () => {
     { name: "Aditya Kumar", github: "https://github.com/adityaK87" },
     { name: "Sarthak Sharma", github: "https://github.com/SuperALKALINEdroiD" },
     { name: "Yassine Ait Rahou", github: "https://github.com/Yassin-Aru" },
-    { name: "Harjot Singh", github: "https://github.com/HarjjotSinghh" }
-
     // Add your name if you are raising your PR
   ];
 
